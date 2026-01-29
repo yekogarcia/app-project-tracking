032fa77f1d60beb51d382b6e709fb788eada47c3
yekogarcia <yekogarcia@yahoo.com>
2026-01-28 23:57:44 -0500

    se quita los tokens

--- src/services/hooks/useFetch.ts
+++ src/services/hooks/useFetch.ts
@@ -1,14 +1,12 @@
 
 
 export const useFetch = async (path: string, options: RequestInit = {}) => {
-    const token = import.meta.env.VITE_URL_TOKEN || '';
     const apiUrl = import.meta.env.VITE_URL_API;
     try {
         const response = await fetch(`${apiUrl}${path}`, {
             ...options,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
                 ...(options.headers || {})
             },
              credentials: 'include',
